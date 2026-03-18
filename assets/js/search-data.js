59e7ba599ea3ed216a89016a872ec80de6e8cd41
TianshunGu <gutiansgeo@gmail.com>
2026-03-18 10:10:03 +0000

    Deploying to gh-pages from @ TianshunGu/TianshunGu.github.io@969be239f89699fd0b30980a6f8fe5d850f0cf20 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -64,10 +64,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-our-study-on-the-attribution-of-spatially-compounding-heatwave-flood-events-has-been-published-in-journal-of-geophysical-research-atmospheres-nature-index-journal-this-work-advances-the-understanding-of-compound-climate-extremes-by-disentangling-the-mechanisms-driving-the-co-occurrence-of-heatwaves-and-flooding-at-regional-scales-read-more-wechat-article-in-chinese",
-          title: '📄 Our study on the attribution of spatially compounding heatwave–flood events has been...',
-          description: "",
-          section: "News",},{id: "news-invited-by-dr-yan-zhang-i-participated-in-the-gischat-seminar-series-no-26-host-gis-building-and-managing-my-own-platform-using-urban-geography-illumination-a-public-science-communication-platform-on-urban-geography-as-a-case-study-i-shared-the-journey-of-building-and-developing-this-platform-along-with-reflections-on-its-evolution-and-future-direction-full-article-wechat-in-chinese",
+            },},{id: "news-invited-by-dr-yan-zhang-i-participated-in-the-gischat-seminar-series-no-26-host-gis-building-and-managing-my-own-platform-using-urban-geography-illumination-a-public-science-communication-platform-on-urban-geography-as-a-case-study-i-shared-the-journey-of-building-and-developing-this-platform-along-with-reflections-on-its-evolution-and-future-direction-full-article-wechat-in-chinese",
           title: '🎤 Invited by Dr. Yan Zhang, I participated in the GISChat Seminar Series...',
           description: "",
           section: "News",},{
